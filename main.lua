295d3a7bdee1b7f9a7c06761a8ed5f646091d312
Patrick McVeety-Mill <p.mcvtm@gmail.com>
2013-03-21 21:49:46 -0500

    Added a brave hero and keyboard controls to move him

--- main.lua
+++ main.lua
@@ -1,10 +1,29 @@
 require 'tile-maps'
 
+Player = {}
+
 
 function love.load()
 	love.filesystem.load('field-map.lua')()
+	Player = {love.graphics.newImage('assets/brave-hero.png'),0,0}
 end
 
 function love.draw()
 	drawMap()
+	love.graphics.draw(Player[1], Player[2], Player[3], 0, 0.5)
+end
+
+function love.keypressed( key )
+   if key == "right" then
+		Player[2] = Player[2] + 32
+   end
+   if key == "left" then
+		Player[2] = Player[2] - 32
+   end
+   if key == "up" then
+		Player[3] = Player[3] - 32
+   end
+   if key == "down" then
+		Player[3] = Player[3] + 32
+   end
 end
