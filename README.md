78d9cc42fd2ec424e0e2b433e48a40bf63d94111
Patrick McVeety-Mill <p.mcvtm@gmail.com>
2013-03-25 22:03:40 -0500

    things just got love.physicsal

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ first-kiss
 
 playground for trying stuff out in lua with LÖVE.
 
-Currently is straight up [this tutorial](https://github.com/kikito/love-tile-tutorial).
+Tile system is straight up [this tutorial](https://github.com/kikito/love-tile-tutorial).
+Now with some LÖVEly physics learned from [its wiki] (http://love2d.org/wiki/).
--- main.lua
+++ main.lua
@@ -1,29 +1,46 @@
 require 'tile-maps'
 
-Player = {}
-
+local World = {}
+local WorldObjects = {}
+local PlayerSpeed = 300
 
 function love.load()
 	love.filesystem.load('field-map.lua')()
-	Player = {love.graphics.newImage('assets/brave-hero.png'),0,0}
+	loadPhysics()
+	PlayerImage = love.graphics.newImage('assets/brave-hero.png')
+end
+
+function loadPhysics()
+	love.physics.setMeter(32) --32px / m
+	World = love.physics.newWorld(0, 0, true)
+	player = {}
+	player.body = love.physics.newBody(World,64,64, "dynamic")
+	player.shape = love.physics.newRectangleShape(32,32)
+	player.fixture = love.physics.newFixture(player.body, player.shape, 1)
+	
+	WorldObjects.player = player
 end
 
 function love.draw()
 	drawMap()
-	love.graphics.draw(Player[1], Player[2], Player[3], 0, 0.5)
+	love.graphics.draw(PlayerImage, WorldObjects.player.body:getX(), WorldObjects.player.body:getY(), 0, 0.5)
+end
+
+function love.update(dt)
+	World:update(dt)
 end
 
 function love.keypressed( key )
    if key == "right" then
-		Player[2] = Player[2] + 32
+	WorldObjects.player.body:applyForce(PlayerSpeed,0) 
    end
    if key == "left" then
-		Player[2] = Player[2] - 32
+	WorldObjects.player.body:applyForce(-PlayerSpeed,0)
    end
    if key == "up" then
-		Player[3] = Player[3] - 32
+	WorldObjects.player.body:applyForce(0,-PlayerSpeed)
    end
    if key == "down" then
-		Player[3] = Player[3] + 32
+	WorldObjects.player.body:applyForce(0,PlayerSpeed)
    end
 end
